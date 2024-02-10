e93ceba0d65bf45b51620b76b57bb14113bd18a6
kirthisid <33558417+kirthisid@users.noreply.github.com>
2024-02-10 13:36:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+function setup() {
+    canvas = createCanvas(640, 420)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+
+}
+function start() {
+    objectdetecter = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "model is loading"
+
+}
+status = ""
+objects = []
+function draw() {
+    image(video, 0, 0, 640, 420)
+    if (status) {
+
+
+
+        objectdetecter.detect(video, gotresult)
+
+        for (i = 0; i < objects.length; i++) {
+
+            percent = floor(objects[i].confidence * 100)
+            fill("red")
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15)
+            stroke("blue")
+            noFill()
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+}
+function modelLoaded() {
+    console.log("model loaded");
+    status = true
+    document.getElementById("status").innerHTML = "model loaded"
+    objectdetecter.detect(video, gotresult)
+}
+
+function gotresult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        objects = result
+    }
+}
